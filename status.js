f55747d67eafff3f69f24198ab8e0e7ee05c84e3
Kenneth Tyler Mills <ktylermills@gmail.com>
2026-09-25 17:30:30 -0400

    Initial script added: 'status.js'

--- status.js
+++ status.js
@@ -2,7 +2,8 @@
 
 export async function main(ns) {
 
-  const server = ns.args[0]
+  const server = ns.args[0];
+
   // @ts-ignore
   ns.tprint("Server max money: " + ns.getServerMaxMoney(server));
   // @ts-ignore
